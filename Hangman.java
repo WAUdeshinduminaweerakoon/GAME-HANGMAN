09c70c0ded30afb449a37e03a4f0a63aed9fa37f
sarvo178 <ovindupathirana554@gmail.com>
2023-11-17 01:14:02 +0530

    add the scanner in file word Scanning

--- Hangman.java
+++ Hangman.java
@@ -1,9 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Hangman {
-    public static void main(String args[]) {
-        Scanner scanner = new Scanner();
+    public static void main(String args[]) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("C:/Users/Dell/Desktop/Game-Hangman/words_alpha.txt"));
 
+        List<String> words = new ArrayList<>();
+        While(scanner.hasNext());
+        {
+            words.add(scanner.nextLine());
+        }
+    }
+
+    private static void While(boolean hasNext) {
     }
 
 }
