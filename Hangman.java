239da16d07ab2929ddce54137b58f42f8e345647
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2023-11-20 10:39:09 +0530

    add the readWordsFromFile(),lidNumberOfLetters and getRandomWordWithLetters , but not work

--- Hangman.java
+++ Hangman.java
@@ -11,55 +11,60 @@ public class Hangman {
         Scanner keyboardLitter = new Scanner(System.in);
         System.out.println("1 or 2 players(plass enter 1 or 2) : ");
         String numberOfPlayer = keyboardLitter.nextLine();
+        // ----------------------------------------------------------------
+
+        // String word;
+        // if (numberOfPlayer.equals("1")) {
+
+        // Scanner scanner = new Scanner(new
+        // File("C:/Users/Dell/Desktop/Game-Hangman/words_alpha.txt"));
+
+        // List<String> words = new ArrayList<>();
+        // while (scanner.hasNext()) {
+        // words.add(scanner.nextLine());
+        // }
+        // // Close the scanner after reading from the file
+        // scanner.close();
+
+        // System.out.println("Enter the number of letters in the word: ");
+        // int numberOfLetters = keyboardLitter.nextInt();
+
+        // // Select a random word with the specified number of letters
+        // do {
+        // word = words.get(new Random().nextInt(words.size()));
+        // } while (word.length() != numberOfLetters);
+
+        // // Random rand = new Random();*-----------------
+        // // word = words.get(rand.nextInt(words.size()));
+
+        // // System.out.println("enter the number of litter in word : ");
+        // // int numberOflitter = keyboardLitter.nextInt();
+        // // do {
+        // // word = words.get(rand.nextInt(words.size()));
+        // // } while (word.length() != numberOflitter);
+
+        // } else {
+        // System.out.println("Player 1, please enter your word: ");
+        // word = keyboardLitter.nextLine();
+        // System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        // System.out.println("Ready for player 2! Good luck!");
+        // }
+        // System.out.println(word);
 
         String word;
         if (numberOfPlayer.equals("1")) {
+            List<String> words = readWordsFromFile();
 
-            Scanner scanner = new Scanner(new File("C:/Users/Dell/Desktop/Game-Hangman/words_alpha.txt"));
-
-            List<String> words = new ArrayList<>();
-            while (scanner.hasNext()) {
-                words.add(scanner.nextLine());
-            }
-            // Close the scanner after reading from the file
-            scanner.close();
-
-            // Random rand = new Random();
-            // // word = words.get(rand.nextInt(words.size()));
-
-            // System.out.println("enter the number of litter in word : ");
-            // int numberOflitter = keyboardLitter.nextInt();
-            // do {
-            // word = words.get(rand.nextInt(words.size()));
-            // } while (word.length() != numberOflitter);
             System.out.println("Enter the number of letters in the word: ");
-            int numberOfLetters = keyboardLitter.nextInt(); // Read an integer
+            int numberOfLetters = getValidNumberOfLetters(keyboardLitter);
 
-            // List<String> words = /* your list of words */;
-            // String word;
-
-            // Check if the list is not empty and if the word with the desired number of
-            // letters exists
-            do {
-                if (words.isEmpty()) {
-                    System.out.println("No words available in the list.");
-                    return; // or handle it in some way appropriate for your program
-                }
-                word = words.get(new Random().nextInt(words.size()));
-            } while (word.length() != numberOfLetters);
-
-            System.out.println("Selected word: " + word);
-            scanner.close();
-
-            // --------------------------------------------------------
-
+            word = getRandomWordWithLetters(words, numberOfLetters);
         } else {
             System.out.println("Player 1, please enter your word: ");
             word = keyboardLitter.nextLine();
             System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
             System.out.println("Ready for player 2! Good luck!");
         }
-        // System.out.println(word);
 
         List<Character> newplayer = new ArrayList<>();
 
@@ -92,6 +97,33 @@ public class Hangman {
             }
         }
 
+    }
+
+    private static List<String> readWordsFromFile() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("C:/Users/Dell/Desktop/Game-Hangman/words_alpha.txt"));
+        List<String> words = new ArrayList<>();
+        while (scanner.hasNext()) {
+            words.add(scanner.nextLine());
+        }
+        scanner.close();
+        return words;
+    }
+
+    private static int getValidNumberOfLetters(Scanner keyboardLitter) {
+        while (!keyboardLitter.hasNextInt()) {
+            System.out.println("Invalid input. Please enter a valid integer.");
+            keyboardLitter.next(); // Consume the invalid input
+        }
+        return keyboardLitter.nextInt();
+    }
+
+    private static String getRandomWordWithLetters(List<String> words, int numberOfLetters) {
+        Random random = new Random();
+        String selectedWord;
+        do {
+            selectedWord = words.get(random.nextInt(words.size()));
+        } while (selectedWord.length() != numberOfLetters);
+        return selectedWord;
     }
 
     private static void printHangeMan(Integer wrongGuessWordCount) {
